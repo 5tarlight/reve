d86819aa98c72a5a45dc601a6e9695e6db1eb4e9
YEAHx4 <developerstarlight@gmail.com>
2023-02-09 23:35:25 +0900

    show logo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use constants::LOGO;
+
+pub mod constants;
 
 fn main() {
     App::new()
@@ -11,5 +14,47 @@ fn main() {
             },
             ..Default::default()
         }))
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Spawn Logo
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::Center,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                        position_type: PositionType::Absolute,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::FlexStart,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(
+                        TextBundle::from_section(
+                            LOGO,
+                            TextStyle {
+                                font_size: 100.,
+                                color: Color::WHITE,
+                                font: asset.load("fonts/d2coding.ttf"),
+                            },
+                        )
+                        .with_text_alignment(TextAlignment::TOP_CENTER),
+                    );
+                });
+        });
 }
