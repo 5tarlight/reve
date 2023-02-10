7b35058fc32fb2e041359ab937aa99735a0c551a
YEAHx4 <developerstarlight@gmail.com>
2023-02-11 04:06:17 +0900

    ip logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.path}`);
+  console.log(`[${req.ip}][${req.method}] ${req.path}`);
   next();
 });
 
