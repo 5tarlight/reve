dffea3c0f603c54028a4e760e0d48bd39ea318db
YEAHx4 <developerstarlight@gmail.com>
2023-02-11 01:13:42 +0900

    cors, body-parser init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,19 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { login, register } from "./auth";
 
 const app = express();
+
+app.use(cors);
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/register", register);
+app.post("/login", login);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
