ca946b6f66583a4699b38a05a732cdf6c151a049
YEAHx4 <developerstarlight@gmail.com>
2023-02-13 22:55:18 +0900

    prevent camera from getting out of map

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::{Handle, Image, Resource};
 
+pub const MIN_MAP_X: f32 = -5140.0;
+pub const MIN_MAP_Y: f32 = -4580.0;
+pub const MAX_MAP_X: f32 = 5140.0;
+pub const MAX_MAP_Y: f32 = 4580.0;
+
 pub const LOGO: &str = "REVE";
 pub const MAP: &str = "img/map.png";
 pub const GAREN: &str = "img/champ/Garen.png";
--- src/input.rs
+++ src/input.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::constants::{MAX_MAP_X, MAX_MAP_Y, MIN_MAP_X, MIN_MAP_Y};
+
 pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
@@ -9,17 +11,19 @@ impl Plugin for InputPlugin {
 }
 
 fn move_map(key: Res<Input<KeyCode>>, mut cam_query: Query<&mut Transform, With<Camera2d>>) {
+    // TODO : Movement speed multiplier
+
     if key.pressed(KeyCode::Left) {
         let mut cam = cam_query.get_single_mut().unwrap();
-        cam.translation.x -= 20.;
+        cam.translation.x = MIN_MAP_X.max(cam.translation.x - 20.);
     } else if key.pressed(KeyCode::Right) {
         let mut cam = cam_query.get_single_mut().unwrap();
-        cam.translation.x += 20.;
+        cam.translation.x = MAX_MAP_X.min(cam.translation.x + 20.);
     } else if key.pressed(KeyCode::Up) {
         let mut cam = cam_query.get_single_mut().unwrap();
-        cam.translation.y += 20.;
+        cam.translation.y = MAX_MAP_Y.min(cam.translation.y + 20.);
     } else if key.pressed(KeyCode::Down) {
         let mut cam = cam_query.get_single_mut().unwrap();
-        cam.translation.y -= 20.;
+        cam.translation.y = MIN_MAP_Y.max(cam.translation.y - 20.);
     }
 }
